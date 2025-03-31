bac7e21806317dc34fac79b9238dea7c75015d30
Sujal Kalra <114348922+sujalkalra@users.noreply.github.com>
2025-03-31 22:48:59 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,11 +16,10 @@ const Navbar = ({ setUsers }) => {
 						gap={3}
 						display={{ base: "none", sm: "flex" }}
 					>
-						<img src='/react.png' alt='React logo' width={50} height={50} />
+						<img src='/dev.png' alt='dev logo' width={50} height={50} />
 						<Text fontSize={"40px"}>+</Text>
-						<img src='/python.png' alt='Python logo' width={50} height={40} />
+						<img src='/dev.png' alt='dev logo' width={50} height={40} />
 						<Text fontSize={"40px"}>=</Text>
-
 						<img src='/explode.png' alt='Explode head' width={45} height={45} />
 					</Flex>
 					{/* Right side */}
